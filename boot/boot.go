4cdd5f394bf668fd22c4e91ac2f220cae2f702a7
ssrs100 <18126523585@163.com>
2018-10-30 17:11:47 +0800

    Update boot.go

--- boot/boot.go
+++ boot/boot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"estor"
 	"estor/utils"
 	"flag"
 	"log"
@@ -10,9 +11,11 @@ import (
 func main() {
 	var ver bool
 	flag.BoolVar(&ver, "version", false, "print version.")
+	flag.Parse()
 	if ver {
 		utils.PrintVersion()
 		os.Exit(0)
 	}
+	estor.Start()
 	log.Println("Proc finished normally.")
 }
